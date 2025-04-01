c855151db05ead6b47a24ee69bbe84e250558f62
Yuliia <yuliadzyoba@gmail.com>
2025-04-01 23:09:22 +0300

    add fixing reviews

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
-import glob from 'glob';
+import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import { globalStylesOptions } from './global.styles';
+import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
@@ -21,36 +20,29 @@ export default defineConfig(({ command }) => {
               return 'vendor';
             }
           },
-          entryFileNames: 'commonHelpers.js',
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
         },
       },
       outDir: '../dist',
+      emptyOutDir: true,
     },
     plugins: [
       injectHTML(),
       FullReload(['./src/**/**.html']),
-      ViteImageOptimizer({
-        exclude: /^sprite.svg$/,
-        png: {
-          quality: 60,
-        },
-        jpeg: {
-          quality: 60,
-        },
-        jpg: {
-          quality: 60,
-        },
-        webp: {
-          quality: 60,
-        },
+      SortCss({
+        sort: 'mobile-first',
       }),
     ],
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: globalStylesOptions,
-        },
-      },
-    },
   };
 });
