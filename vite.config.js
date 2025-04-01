3369fe68222dd4c03bded26660b315872c69c6ff
Yuliia <yuliadzyoba@gmail.com>
2025-04-01 23:12:29 +0300

    add fixing vite.config,js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
+import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { globalStylesOptions } from './global.styles';
 
 export default defineConfig(({ command }) => {
   return {
@@ -12,6 +13,7 @@ export default defineConfig(({ command }) => {
     root: 'src',
     build: {
       sourcemap: true,
+
       rollupOptions: {
         input: glob.sync('./src/*.html'),
         output: {
@@ -20,29 +22,36 @@ export default defineConfig(({ command }) => {
               return 'vendor';
             }
           },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
+          entryFileNames: 'commonHelpers.js',
         },
       },
       outDir: '../dist',
-      emptyOutDir: true,
     },
     plugins: [
       injectHTML(),
       FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
+      ViteImageOptimizer({
+        exclude: /^sprite.svg$/,
+        png: {
+          quality: 60,
+        },
+        jpeg: {
+          quality: 60,
+        },
+        jpg: {
+          quality: 60,
+        },
+        webp: {
+          quality: 60,
+        },
       }),
     ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: globalStylesOptions,
+        },
+      },
+    },
   };
 });
