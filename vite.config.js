224ebd27ff2ee9a60ac05700ec3a7c14b86e5626
Yuliia <yuliadzyoba@gmail.com>
2025-04-02 03:04:27 +0300

    add fixing config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/Potential/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
